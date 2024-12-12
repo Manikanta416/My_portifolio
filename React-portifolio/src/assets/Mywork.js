c10f8b6bb0571bab2c3bd9be1c6367ad8549936f
Mutta Manikanta Swamy <143594090+Manikanta416@users.noreply.github.com>
2024-12-12 23:39:23 +0530

    commit
    just commited

---
+++ React-portifolio/src/assets/Mywork.js
@@ -0,0 +1,22 @@
+import Weather from '../assets/Weather.png'
+import Digitease from '../assets/Digitease.png'
+import smt from '../assets/smt.jpg'
+
+
+const Mywork_Data=[
+    {
+        s_no:"1",
+        s_name:"React-Web design",
+        s_img:Weather
+    },
+    {
+        s_no:"2",
+        s_name:"Digit ease",
+       s_img:Digitease
+    }, {
+        s_no:"3",
+        s_name:"Smt Prototype",
+        s_img:smt
+    }
+]
+export default Mywork_Data
